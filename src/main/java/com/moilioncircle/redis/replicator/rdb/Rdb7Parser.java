fc7566e81dacbc4a4f4e41a3f197c22f853f2a2c
leon <chen.bao.yi@qq.com>
2016-08-23 23:02:28 +0800

    remove parse() method in Rdb7Parser

--- src/main/java/com/moilioncircle/redis/replicator/rdb/Rdb7Parser.java
+++ src/main/java/com/moilioncircle/redis/replicator/rdb/Rdb7Parser.java
@@ -19,56 +19,6 @@ public class Rdb7Parser extends AbstractRdbParser {
         super(in, replicator);
     }
 
-    /**
-     * ----------------------------# RDB is a binary format. There are no new lines or spaces in the file.
-     * 52 45 44 49 53              # Magic String "REDIS"
-     * 30 30 30 33                 # RDB Version Number in big endian. In this case, version = 0003 = 3
-     * ----------------------------
-     * FE 00                       # FE = code that indicates database selector. db number = 00
-     * ----------------------------# Key-Value pair starts
-     * FD $unsigned int            # FD indicates "expiry time in seconds". After that, expiry time is read as a 4 byte unsigned int
-     * $value-type                 # 1 byte flag indicating the type of value - set, map, sorted set etc.
-     * $string-encoded-name         # The name, encoded as a redis string
-     * $encoded-value              # The value. Encoding depends on $value-type
-     * ----------------------------
-     * FC $unsigned long           # FC indicates "expiry time in ms". After that, expiry time is read as a 8 byte unsigned long
-     * $value-type                 # 1 byte flag indicating the type of value - set, map, sorted set etc.
-     * $string-encoded-name         # The name, encoded as a redis string
-     * $encoded-value              # The value. Encoding depends on $value-type
-     * ----------------------------
-     * $value-type                 # This name value pair doesn't have an expiry. $value_type guaranteed != to FD, FC, FE and FF
-     * $string-encoded-name
-     * $encoded-value
-     * ----------------------------
-     * FE $length-encoding         # Previous db ends, next db starts. Database number read using length encoding.
-     * ----------------------------
-     * ...                         # Key value pairs for this database, additonal database
-     * FF                          ## End of RDB file indicator
-     * 8 byte checksum             ## CRC 64 checksum of the entire file.
-     *
-     * @return read bytes
-     * @throws IOException when read timeout
-     */
-    public long parse() throws IOException {
-        /*
-         * ----------------------------
-         * 52 45 44 49 53              # Magic String "REDIS"
-         * 30 30 30 33                 # RDB Version Number in big endian. In this case, version = 0003 = 3
-         * ----------------------------
-         */
-        String magicString = AbstractRdbParser.StringHelper.str(in, 5);//REDIS
-        if (!magicString.equals("REDIS")) {
-            logger.error("Can't read MAGIC STRING [REDIS] ,value:" + magicString);
-            return in.total();
-        }
-        int version = Integer.parseInt(AbstractRdbParser.StringHelper.str(in, 4));//0006
-        if (version < 1 || version > 7) {
-            logger.error("Can't handle RDB format version " + version);
-            return in.total();
-        }
-        return rdbLoad();
-    }
-
     protected long rdbLoad() throws IOException {
         Db db = null;
         /**

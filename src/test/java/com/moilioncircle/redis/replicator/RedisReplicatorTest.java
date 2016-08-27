3631337f781b5590e4e31c2656f1e8866a415a79
leon <chen.bao.yi@qq.com>
2016-08-27 23:44:30 +0800

    1.0.6-SNAPSHOT test case

--- src/test/java/com/moilioncircle/redis/replicator/RedisReplicatorTest.java
+++ src/test/java/com/moilioncircle/redis/replicator/RedisReplicatorTest.java
@@ -17,19 +17,21 @@
 package com.moilioncircle.redis.replicator;
 
 import com.moilioncircle.redis.replicator.cmd.Command;
+import com.moilioncircle.redis.replicator.cmd.CommandFilter;
 import com.moilioncircle.redis.replicator.cmd.CommandListener;
-import com.moilioncircle.redis.replicator.rdb.RdbListener;
-import com.moilioncircle.redis.replicator.rdb.datatype.KeyValuePair;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import com.moilioncircle.redis.replicator.cmd.CommandName;
+import com.moilioncircle.redis.replicator.cmd.impl.SetParser;
+import junit.framework.TestCase;
 import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import java.io.IOException;
 
 /**
  * Created by leon on 8/13/16.
  */
-public class RedisReplicatorTest {
+public class RedisReplicatorTest extends TestCase {
 
-    private static final Log logger = LogFactory.getLog(RedisSocketReplicator.class);
 
     @Test
     public void testSync() throws Exception {
@@ -37,31 +39,46 @@ public class RedisReplicatorTest {
         final RedisReplicator replicator = new RedisReplicator("127.0.0.1",
                 6379,
                 Configuration.defaultSetting()
-                        .setAuthPassword("test")
                         .setRetries(0)
                         .setVerbose(true));
-        replicator.addRdbListener(new RdbListener.Adaptor() {
-            @Override
-            public void handle(Replicator replicator, KeyValuePair<?> kv) {
+        new Thread() {
+            public void run() {
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(5000);
+                    Jedis jedis = new Jedis("127.0.0.1", 6379);
+                    jedis.set("abc", "bcd");
+                    jedis.close();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                logger.debug(kv);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    replicator.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+        replicator.addCommandFilter(new CommandFilter() {
+            @Override
+            public boolean accept(Command command) {
+                return command.name().equals(CommandName.name("SET"));
             }
         });
         replicator.addCommandListener(new CommandListener() {
             @Override
             public void handle(Replicator replicator, Command command) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                logger.debug(command);
+                SetParser.SetCommand setCommand = (SetParser.SetCommand) command;
+                assertEquals("abc", setCommand.key);
+                assertEquals("bcd", setCommand.value);
+                System.out.println("done");
             }
         });
         replicator.open();
+
     }
 }

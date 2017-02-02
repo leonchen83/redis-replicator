7db33a1edbfb6addd08d7dd29bd613e0f6141167
leon <chen.bao.yi@qq.com>
2017-02-02 16:26:40 +0800

    2.0.0 release

--- src/main/java/com/moilioncircle/redis/replicator/Constants.java
+++ src/main/java/com/moilioncircle/redis/replicator/Constants.java
@@ -24,7 +24,11 @@ import java.nio.charset.Charset;
  */
 public class Constants {
 
-    public static final char[] MODULE_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".toCharArray();
+    public static final char[] MODULE_SET = new char[]{
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'
+    };
 
     public static final Charset CHARSET = Charset.forName("UTF-8");
     public static final String EMPTY = "";

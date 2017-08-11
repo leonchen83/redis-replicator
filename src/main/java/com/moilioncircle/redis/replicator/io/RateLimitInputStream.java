4ce9c7b66e35ada75621d95c7fabe2fce3fa6024
leon <chen.bao.yi@qq.com>
2017-08-11 22:57:35 +0800

    new rate limit support

--- src/main/java/com/moilioncircle/redis/replicator/io/RateLimitInputStream.java
+++ src/main/java/com/moilioncircle/redis/replicator/io/RateLimitInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 Leon Chen
+ * Copyright 2016 leon chen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,14 +19,8 @@ package com.moilioncircle.redis.replicator.io;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -34,59 +28,33 @@ import static java.lang.System.currentTimeMillis;
  * @author Leon Chen
  * @since 2.3.2
  */
-public class RateLimitInputStream extends InputStream implements Runnable {
-
+public class RateLimitInputStream extends InputStream {
     private static final Log logger = LogFactory.getLog(RateLimitInputStream.class);
 
     private static final int DEFAULT_PERMITS = 100 * 1024 * 1000; // 97.65MB/sec
 
     private final int permits;
-    private final Thread worker;
+    private RateLimiter limiter;
     private final InputStream in;
-    private final RateLimiter limiter;
-    private final ThreadFactory factory;
-    private volatile IOException exception;
-    private final ReentrantLock lock = new ReentrantLock();
-    private final Condition reader = this.lock.newCondition();
-    private final Condition writer = this.lock.newCondition();
-    private final AtomicBoolean closed = new AtomicBoolean(false);
 
     public RateLimitInputStream(InputStream in) {
         this(in, DEFAULT_PERMITS);
     }
 
     public RateLimitInputStream(InputStream in, int permits) {
-        this(in, permits, Executors.defaultThreadFactory());
-    }
-
-    public RateLimitInputStream(InputStream in, int permits, ThreadFactory factory) {
         if (permits <= 1000) permits = 1000;
         else if (permits > 1000) permits = permits / 1000 * 1000;
         logger.info("rate limit force set to " + permits);
-        //
+
         this.in = in;
-        this.factory = factory;
         this.permits = permits;
         this.limiter = new TokenBucketRateLimiter(this.permits);
-        this.worker = this.factory.newThread(this);
-        this.worker.start();
     }
 
     @Override
     public int read() throws IOException {
-        lock.lock();
-        try {
-            while (!limiter.acquire(1)) {
-                if (this.exception != null) throw this.exception;
-                this.reader.awaitUninterruptibly();
-                if (this.closed.get()) throw new EOFException();
-            }
-            int r = in.read();
-            this.writer.signalAll();
-            return r;
-        } finally {
-            lock.unlock();
-        }
+        limiter.acquire(1);
+        return in.read();
     }
 
     @Override
@@ -96,52 +64,32 @@ public class RateLimitInputStream extends InputStream implements Runnable {
 
     @Override
     public int read(byte[] b, int offset, int length) throws IOException {
-        lock.lock();
-        try {
-            int total = length, index = offset;
-            while (total > 0) {
-                int len = Math.min(permits, total);
-                while (!limiter.acquire(len)) {
-                    if (this.exception != null) throw this.exception;
-                    this.reader.awaitUninterruptibly();
-                    if (this.closed.get()) throw new EOFException();
-                }
-                int r = in.read(b, index, len);
-                this.writer.signalAll();
-                index += r;
-                total -= r;
-                if (r < 0) return r;
-                if (r < len) return length - total;
-            }
-            assert total == 0;
-            return length;
-        } finally {
-            lock.unlock();
+        int total = length, index = offset;
+        while (total > 0) {
+            int len = Math.min(permits, total);
+            limiter.acquire(len);
+            int r = in.read(b, index, len);
+            index += r;
+            total -= r;
+            if (r < 0) return r;
+            if (r < len) return length - total;
         }
+        assert total == 0;
+        return length;
     }
 
     @Override
     public long skip(long length) throws IOException {
-        lock.lock();
-        try {
-            long total = length;
-            while (total > 0) {
-                int skip = (int) Math.min(permits, total);
-                while (!limiter.acquire(skip)) {
-                    if (this.exception != null) throw this.exception;
-                    this.reader.awaitUninterruptibly();
-                    if (this.closed.get()) throw new EOFException();
-                }
-                long r = in.skip(skip);
-                this.writer.signalAll();
-                total -= r;
-                if (r < skip) return length - total;
-            }
-            assert total == 0;
-            return length;
-        } finally {
-            lock.unlock();
+        long total = length;
+        while (total > 0) {
+            int skip = (int) Math.min(permits, total);
+            limiter.acquire(skip);
+            long r = in.skip(skip);
+            total -= r;
+            if (r < skip) return length - total;
         }
+        assert total == 0;
+        return length;
     }
 
     @Override
@@ -151,120 +99,52 @@ public class RateLimitInputStream extends InputStream implements Runnable {
 
     @Override
     public void close() throws IOException {
-        if (!this.closed.compareAndSet(false, true)) return;
-        try {
-            this.in.close();
-        } finally {
-            this.lock.lock();
-            try {
-                this.reader.signalAll();
-                this.writer.signalAll();
-            } finally {
-                this.lock.unlock();
-            }
-        }
-    }
-
-    @Override
-    public void run() {
-        try {
-            int yield = 40, idx = 0;
-            while (!this.closed.get()) {
-                this.lock.lock();
-                try {
-                    while (limiter.full()) {
-                        this.writer.awaitUninterruptibly();
-                        if (this.closed.get()) throw new EOFException();
-                    }
-                    while (!limiter.update()) {
-                        if (idx++ >= yield) {
-                            idx = 0;
-                            Thread.yield();
-                        }
-                    }
-                    if (lock.hasWaiters(this.reader))
-                        this.reader.signalAll();
-                } finally {
-                    this.lock.unlock();
-                }
-            }
-        } catch (IOException e) {
-            this.exception = e;
-        } finally {
-            if (!this.closed.get()) {
-                try {
-                    close();
-                } catch (IOException e) {
-                    logger.error("failed to close is", e);
-                }
-            }
-        }
+        this.in.close();
     }
 
     private interface RateLimiter {
-
-        /**
-         * @return return whether bucket is full
-         * @see {@link RateLimitInputStream#run()}
-         */
-        boolean full();
-
-        /**
-         * @return release() >= gap
-         * @see {@link RateLimitInputStream#run()}
-         */
-        boolean update();
-
-        /**
-         * @param permits the permits that acquire
-         * @return if has enough permits return true else return false
-         * @see {@link RateLimitInputStream#read()}
-         * @see {@link RateLimitInputStream#read(byte[])}
-         * @see {@link RateLimitInputStream#read(byte[], int, int)}
-         * @see {@link RateLimitInputStream#skip(long)}
-         */
-        boolean acquire(long permits);
+        void acquire(long permits);
     }
 
     private class TokenBucketRateLimiter implements RateLimiter {
 
-        private long gap;
         private long access;
+        private long borrow;
         private long permits;
         private final long size;
+        private final double sleep;
 
         private TokenBucketRateLimiter(int permits) {
             this.access = currentTimeMillis();
             this.size = this.permits = permits;
+            this.sleep = 1 * this.size / 1000L;
         }
 
         @Override
-        public boolean full() {
-            return permits == size;
-        }
-
-        @Override
-        public boolean update() {
-            double p = generate();
-            if (p < this.gap) {
-                this.gap -= p;
-                return false;
-            } else {
-                this.gap = 0;
-                return true;
-            }
-        }
-
-        @Override
-        public boolean acquire(long permits) {
-            generate();
-            if (this.permits < permits) {
-                this.gap = permits - this.permits;
-                return false;
-            } else {
-                this.permits -= permits;
-                this.gap = 0;
-                return true;
+        public void acquire(long permits) {
+            try {
+                while (true) {
+                    generate();
+                    if (this.permits < permits) {
+                        permits -= this.permits;
+                    } else {
+                        this.permits -= permits;
+                        return;
+                    }
+                    double r = permits / sleep;
+                    if (r < 1) {
+                        this.borrow += permits;
+                        while (this.borrow >= sleep) {
+                            Thread.sleep(1);
+                            this.borrow -= sleep;
+                        }
+                        return;
+                    } else {
+                        Thread.sleep((int) r);
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
 

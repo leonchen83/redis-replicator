07899c06603b60fcae8eba0a29c98c2f31b2df84
chenby <chenby@nextop.cn>
2018-06-12 00:06:31 +0800

    redis 5.0

---
+++ src/main/java/com/moilioncircle/redis/replicator/cmd/impl/XGroupDelGroupCommand.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016-2018 Leon Chen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.moilioncircle.redis.replicator.cmd.impl;
+
+/**
+ * @author Leon Chen
+ * @since 2.6.0
+ */
+public class XGroupDelGroupCommand extends XGroupCommand {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private String key;
+    
+    private String group;
+    
+    private byte[] rawKey;
+    
+    private byte[] rawGroup;
+    
+    public XGroupDelGroupCommand() {
+    
+    }
+    
+    public XGroupDelGroupCommand(String key, String group) {
+        this(key, group, null, null);
+    }
+    
+    public XGroupDelGroupCommand(String key, String group, byte[] rawKey, byte[] rawGroup) {
+        this.key = key;
+        this.group = group;
+        this.rawKey = rawKey;
+        this.rawGroup = rawGroup;
+    }
+    
+    public String getKey() {
+        return key;
+    }
+    
+    public void setKey(String key) {
+        this.key = key;
+    }
+    
+    public String getGroup() {
+        return group;
+    }
+    
+    public void setGroup(String group) {
+        this.group = group;
+    }
+    
+    public byte[] getRawKey() {
+        return rawKey;
+    }
+    
+    public void setRawKey(byte[] rawKey) {
+        this.rawKey = rawKey;
+    }
+    
+    public byte[] getRawGroup() {
+        return rawGroup;
+    }
+    
+    public void setRawGroup(byte[] rawGroup) {
+        this.rawGroup = rawGroup;
+    }
+    
+    @Override
+    public String toString() {
+        return "XGroupDelGroupCommand{" +
+                "key='" + key + '\'' +
+                ", group='" + group + '\'' +
+                '}';
+    }
+}

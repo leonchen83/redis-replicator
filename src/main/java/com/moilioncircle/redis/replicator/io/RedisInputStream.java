e69ed3e9a25d6d02d7803d7d07e6a93c4d2a5ce9
chenby <chenby@nextop.cn>
2019-02-21 14:28:32 +0800

    fix offset bug

--- src/main/java/com/moilioncircle/redis/replicator/io/RedisInputStream.java
+++ src/main/java/com/moilioncircle/redis/replicator/io/RedisInputStream.java
@@ -80,7 +80,7 @@ public class RedisInputStream extends InputStream {
         return buf.length;
     }
 
-    public boolean isMark() {
+    public boolean isMarked() {
         return mark;
     }
 

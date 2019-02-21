e05130a48db4df467a7dd2001578a3bac45739cf
chenby <chenby@nextop.cn>
2019-02-21 21:33:48 +0800

    fix offset bug

--- src/main/java/com/moilioncircle/redis/replicator/RedisSocketReplicator.java
+++ src/main/java/com/moilioncircle/redis/replicator/RedisSocketReplicator.java
@@ -391,7 +391,6 @@ public class RedisSocketReplicator extends AbstractReplicator {
                     }
                     if (isEquals(Strings.toString(raw[0]), "PING")) {
                         // NOP
-                        logger.info("print +ping\\r\\n" + ", 7");
                     } else if (isEquals(Strings.toString(raw[0]), "REPLCONF") && isEquals(Strings.toString(raw[1]), "GETACK")) {
                         if (mode == PSYNC) executor.execute(new Runnable() {
                             @Override

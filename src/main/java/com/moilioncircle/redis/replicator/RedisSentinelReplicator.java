d5a5d7d3ca4cbc161ef436978bc99054e4f8a228
chenby <chenby@nextop.cn>
2019-04-01 21:32:01 +0800

    sentinel

--- src/main/java/com/moilioncircle/redis/replicator/RedisSentinelReplicator.java
+++ src/main/java/com/moilioncircle/redis/replicator/RedisSentinelReplicator.java
@@ -18,8 +18,17 @@ package com.moilioncircle.redis.replicator;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+import com.moilioncircle.redis.replicator.cmd.Command;
+import com.moilioncircle.redis.replicator.cmd.CommandName;
+import com.moilioncircle.redis.replicator.cmd.CommandParser;
+import com.moilioncircle.redis.replicator.event.EventListener;
+import com.moilioncircle.redis.replicator.io.RawByteListener;
+import com.moilioncircle.redis.replicator.rdb.datatype.Module;
+import com.moilioncircle.redis.replicator.rdb.module.ModuleKey;
+import com.moilioncircle.redis.replicator.rdb.module.ModuleParser;
 import com.moilioncircle.redis.replicator.sentienl.DefaultSentinel;
 import com.moilioncircle.redis.replicator.sentienl.Sentinel;
 import com.moilioncircle.redis.replicator.sentienl.SentinelListener;
@@ -52,6 +61,114 @@ public class RedisSentinelReplicator extends AbstractReplicator {
 		this.sentinel.close();
 	}
 
+	@Override
+	public boolean addEventListener(EventListener listener) {
+		if (replicator == null) {
+			return super.addEventListener(listener);
+		} else {
+			return replicator.addEventListener(listener);
+		}
+	}
+
+	@Override
+	public boolean removeEventListener(EventListener listener) {
+		if (replicator == null) {
+			return super.removeEventListener(listener);
+		} else {
+			return replicator.removeEventListener(listener);
+		}
+	}
+
+	@Override
+	public boolean addRawByteListener(RawByteListener listener) {
+		if (replicator == null) {
+			return super.addRawByteListener(listener);
+		} else {
+			return replicator.addRawByteListener(listener);
+		}
+	}
+
+	@Override
+	public boolean removeRawByteListener(RawByteListener listener) {
+		if (replicator == null) {
+			return super.removeRawByteListener(listener);
+		} else {
+			return replicator.removeRawByteListener(listener);
+		}
+	}
+
+	@Override
+	public boolean addCloseListener(CloseListener listener) {
+		if (replicator == null) {
+			return super.addCloseListener(listener);
+		} else {
+			return replicator.addCloseListener(listener);
+		}
+	}
+
+	@Override
+	public boolean removeCloseListener(CloseListener listener) {
+		if (replicator == null) {
+			return super.removeCloseListener(listener);
+		} else {
+			return replicator.removeCloseListener(listener);
+		}
+	}
+
+	@Override
+	public boolean addExceptionListener(ExceptionListener listener) {
+		if (replicator == null) {
+			return super.addExceptionListener(listener);
+		} else {
+			return replicator.addExceptionListener(listener);
+		}
+	}
+
+	@Override
+	public boolean removeExceptionListener(ExceptionListener listener) {
+		if (replicator == null) {
+			return super.removeExceptionListener(listener);
+		} else {
+			return replicator.removeExceptionListener(listener);
+		}
+	}
+
+	@Override
+	public CommandParser<? extends Command> getCommandParser(CommandName command) {
+		if (replicator == null) {
+			return super.getCommandParser(command);
+		} else {
+			return replicator.getCommandParser(command);
+		}
+	}
+
+	@Override
+	public <T extends Command> void addCommandParser(CommandName command, CommandParser<T> parser) {
+		if (replicator == null) {
+			super.addCommandParser(command, parser);
+		} else {
+			replicator.addCommandParser(command, parser);
+		}
+	}
+
+	@Override
+	public CommandParser<? extends Command> removeCommandParser(CommandName command) {
+		if (replicator == null) {
+			return super.removeCommandParser(command);
+		} else {
+			return replicator.removeCommandParser(command);
+		}
+	}
+
+	@Override
+	public Status getStatus() {
+		if (replicator == null) {
+			return Status.DISCONNECTED;
+		} else {
+			return replicator.getStatus();
+		}
+	}
+
 	private class DefaultSentinelListener implements SentinelListener {
 
 		@Override
@@ -62,8 +179,26 @@ public class RedisSentinelReplicator extends AbstractReplicator {
 		@Override
 		public void onSwitch(Sentinel sentinel, HostAndPort host) {
 			if (replicator != null) Replicators.close(replicator);
-			replicator = new RedisReplicator(host.getHost(), host.getPort(), getConfiguration());
-			
+			Replicator next = new RedisSocketReplicator(host.getHost(), host.getPort(), getConfiguration());
+			if (replicator == null) {
+				// restore listeners
+				for (CloseListener listener : closeListeners) next.addCloseListener(listener);
+				for (EventListener listener : eventListeners) next.addEventListener(listener);
+				for (RawByteListener listener : rawByteListeners) next.addRawByteListener(listener);
+				for (ExceptionListener listener : exceptionListeners) next.addExceptionListener(listener);
+				// restore module
+				for (Map.Entry<ModuleKey, ModuleParser<? extends Module>> entry : modules.entrySet()) {
+					final ModuleKey key = entry.getKey();
+					next.addModuleParser(key.getModuleName(), key.getModuleVersion(), entry.getValue());
+				}
+				// restore command
+				for (Map.Entry<CommandName, CommandParser<? extends Command>> entry : commands.entrySet()) {
+					next.addCommandParser(entry.getKey(), entry.getValue());
+				}
+				// restore rdb visitor
+				next.setRdbVisitor(rdbVisitor); //TODO how replace replicator in rdbVisitor??
+			}
+			replicator = next;
 			Replicators.open(replicator);
 		}
 	}

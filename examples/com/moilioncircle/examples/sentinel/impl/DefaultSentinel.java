1ab6bad8a9b726ae7ee5c493f7ae6ef4befb3042
Baoyi Chen <chen.bao.yi@qq.com>
2021-07-16 10:39:40 +0800

    examples

--- examples/com/moilioncircle/examples/sentinel/impl/DefaultSentinel.java
+++ examples/com/moilioncircle/examples/sentinel/impl/DefaultSentinel.java
@@ -22,11 +22,19 @@ import static java.lang.Integer.parseInt;
 import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static redis.clients.jedis.Protocol.Command.UNSUBSCRIBE;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,11 +54,13 @@ import redis.clients.jedis.exceptions.JedisException;
 public class DefaultSentinel implements Sentinel {
 
     protected static final Logger logger = LoggerFactory.getLogger(DefaultSentinel.class);
-
+    
+    protected volatile Jedis jedis;
     protected final String masterName;
     protected final List<HostAndPort> hosts;
     protected final Configuration configuration;
     protected final String channel = "+switch-master";
+    protected AtomicBoolean running = new AtomicBoolean(true);
     protected final List<SentinelListener> listeners = new CopyOnWriteArrayList<>();
     protected final ScheduledExecutorService schedule = newSingleThreadScheduledExecutor();
 
@@ -62,13 +72,13 @@ public class DefaultSentinel implements Sentinel {
 
     @Override
     public void open() throws IOException {
-        schedule.scheduleWithFixedDelay(this::pulse, 0, 10, SECONDS);
+        await(schedule.scheduleWithFixedDelay(this::pulse, 0, 10, SECONDS));
     }
 
     @Override
     public void close() throws IOException {
-        terminateQuietly(schedule, configuration.getConnectionTimeout(), MILLISECONDS);
-        doCloseListener();
+        unsubscribe(channel);
+        terminateQuietly(schedule, 0, MILLISECONDS);
     }
 
     @Override
@@ -97,6 +107,7 @@ public class DefaultSentinel implements Sentinel {
 
     protected void pulse() {
         for (HostAndPort sentinel : hosts) {
+            if (!this.running.get()) continue;
             try (final Jedis jedis = new Jedis(sentinel)) {
                 List<String> list = jedis.sentinelGetMasterAddrByName(masterName);
                 if (list == null || list.size() != 2) {
@@ -105,7 +116,7 @@ public class DefaultSentinel implements Sentinel {
                 String host = list.get(0);
                 int port = Integer.parseInt(list.get(1));
                 doSwitchListener(new HostAndPort(host, port));
-
+                this.jedis = jedis;
                 logger.info("subscribe sentinel {}", sentinel);
                 jedis.subscribe(new PubSub(), this.channel);
             } catch (Throwable cause) {
@@ -115,6 +126,13 @@ public class DefaultSentinel implements Sentinel {
     }
 
     protected class PubSub extends JedisPubSub {
+        
+        @Override
+        public void onUnsubscribe(String channel, int channels) {
+            running.set(false);
+            doCloseListener();
+        }
+        
         @Override
         public void onMessage(String channel, String response) {
             try {
@@ -135,6 +153,55 @@ public class DefaultSentinel implements Sentinel {
             } catch (Throwable cause) {
                 logger.error("failed to subscribe: {}, cause: {}", response, cause);
             }
+        }
+    }
+    
+    protected void unsubscribe(String channel) {
+        for (int retry = 0; retry < 5; retry++) {
+            if(!running.get()) break;
+            if(jedis == null || !jedis.isConnected()) continue;
+            run(() -> jedis.sendCommand(UNSUBSCRIBE, channel));
+            sleep(1, SECONDS);
+        }
+    }
+    
+    protected <T> T run(Callable<T> callable) {
+        try {
+            return callable.call();
+        } catch (Exception ignore) {
+            return null;
+        }
+    } 
+    
+    protected void sleep(long time, TimeUnit unit) {
+        try {
+            unit.sleep(time);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    
+    protected void await(Future<?> future) {
+        try {
+            future.get();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (CancellationException e) {
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    
+    protected void await(Future<?> future, long timeout, TimeUnit unit) {
+        try {
+            future.get(timeout, unit);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (CancellationException e) {
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }

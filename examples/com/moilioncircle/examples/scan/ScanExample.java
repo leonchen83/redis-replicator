964258a5ca7ab0c9e8f292d804b39a11d5e9cdba
Baoyi Chen <chen.bao.yi@qq.com>
2023-01-18 18:18:43 +0800

    scan mode

--- examples/com/moilioncircle/examples/scan/ScanExample.java
+++ examples/com/moilioncircle/examples/scan/ScanExample.java
@@ -16,10 +16,22 @@
 
 package com.moilioncircle.examples.scan;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.moilioncircle.redis.replicator.Configuration;
+import com.moilioncircle.redis.replicator.RedisRdbReplicator;
 import com.moilioncircle.redis.replicator.RedisReplicator;
 import com.moilioncircle.redis.replicator.Replicator;
 import com.moilioncircle.redis.replicator.event.Event;
 import com.moilioncircle.redis.replicator.event.EventListener;
+import com.moilioncircle.redis.replicator.event.PostRdbSyncEvent;
+import com.moilioncircle.redis.replicator.event.PreRdbSyncEvent;
+import com.moilioncircle.redis.replicator.io.RawByteListener;
+import com.moilioncircle.redis.replicator.rdb.skip.SkipRdbVisitor;
 
 /**
  * @author Leon Chen
@@ -27,7 +39,12 @@ import com.moilioncircle.redis.replicator.event.EventListener;
  */
 @SuppressWarnings("resource")
 public class ScanExample {
-    public static void main(String[] args) throws Exception {
+    
+    /**
+     * @throws Exception
+     * scan all redis data
+     */
+    public void scan() throws Exception {
         Replicator replicator = new RedisReplicator("redis://127.0.0.1:6379?enableScan=yes&scanStep=128");
         replicator.addEventListener(new EventListener() {
             @Override
@@ -36,6 +53,55 @@ public class ScanExample {
             }
         });
         
+        replicator.open();
+    }
+    
+    /**
+     * @throws IOException
+     * scan redis and save to rdb file
+     */
+    public void scanToRdb() throws Exception {
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(new File("./dump.rdb")));
+        RawByteListener listener = new RawByteListener() {
+            @Override
+            public void handle(byte... rawBytes) {
+                try {
+                    out.write(rawBytes);
+                } catch (IOException ignore) {
+                }
+            }
+        };
+    
+        //save rdb from remote server
+        Replicator replicator = new RedisReplicator("redis://127.0.0.1:6379?enableScan=yes&scanStep=512");
+        replicator.setRdbVisitor(new SkipRdbVisitor(replicator));
+        replicator.addEventListener(new EventListener() {
+            @Override
+            public void onEvent(Replicator replicator, Event event) {
+                if (event instanceof PreRdbSyncEvent) {
+                    replicator.addRawByteListener(listener);
+                }
+                if (event instanceof PostRdbSyncEvent) {
+                    replicator.removeRawByteListener(listener);
+                    try {
+                        out.close();
+                        replicator.close();
+                    } catch (IOException ignore) {
+                    }
+                }
+            }
+        });
+        
+        replicator.open();
+    
+        //check rdb file
+        replicator = new RedisRdbReplicator(new File("./dump.rdb"), Configuration.defaultSetting());
+        replicator.addEventListener(new EventListener() {
+            @Override
+            public void onEvent(Replicator replicator, Event event) {
+                System.out.println(event);
+            }
+        });
         replicator.open();
     }
 }
